ebf580f3d7ea0c9b00ea6ed61c66cab52d3941a5
tsalokat <tim.salokat@outlook.com>
2026-02-28 17:54:07 +0100

    snap fix

--- src/lib/components/BottomSheet.svelte
+++ src/lib/components/BottomSheet.svelte
@@ -92,8 +92,8 @@
                 }
             }
 
-            currentY = closestPercent * containerHeight;
-            percentage = closestPercent;
+            currentY = closest;
+            percentage = closest / containerHeight;
         }
     }
 
