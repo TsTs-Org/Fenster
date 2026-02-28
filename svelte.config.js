aee278501567760aaa6ea7d6cf6739be923e1003
tsalokat <tim.salokat@outlook.com>
2026-02-28 18:40:38 +0100

    config changes

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,9 @@ const config = {
     adapter: adapter({
       fallback: "index.html",
     }),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/Fenster" : "",
+    },
   },
 };
 
